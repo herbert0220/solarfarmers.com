598240a90ebe180147ac9476b0284d51e5bacaeb
Herbert Huamg <snake19820220@gmail.com>
2025-06-27 09:20:39 +0800

    ?撠汗??摰?批捆?湔

--- 部署小農綠電_最終版.bat
+++ 部署小農綠電_最終版.bat
@@ -1,5 +1,14 @@
 @echo off
+echo === 小農綠電 一鍵部署 開始 ===
+
+:: 設定目前資料夾
 set WORKDIR=%~dp0
-cd /d %WORKDIR%
-start "" "%ProgramFiles%\Git\git-bash.exe" -c "cd \"$(cygpath '%WORKDIR%')\" && git add . && git commit -m '網站最新內容自動部署' && git push origin master"
+cd /d "%WORKDIR%"
+
+echo.
+echo [1] 呼叫 Git Bash 執行 Git 指令...
+"%ProgramFiles%\Git\git-bash.exe" -c "cd \"$(cygpath '%WORKDIR%')\" && git add . && git commit -m '網站更新' && git push origin master"
+
+echo.
+echo === 上傳完成，請稍候刷新網站確認 ===
 pause
