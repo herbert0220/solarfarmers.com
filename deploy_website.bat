0ab3400beaae4bb0452d91bb18c2221c6228e402
Herbert Huamg <snake19820220@gmail.com>
2025-07-02 07:22:37 +0800

    正式版網站更新

--- deploy_website.bat
+++ deploy_website.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d C:\Users\user\Desktop\web
 git add .
-git commit -m "多層正式版網站更新"
+git commit -m "正式完整版網站更新"
 git push origin master
 echo 已更新至 https://herbert0220.github.io/solarfarmers.com/
 pause
