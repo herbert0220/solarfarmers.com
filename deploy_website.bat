3d0b9f02081b73d527a23c73c97f06db10454631
Herbert Huamg <snake19820220@gmail.com>
2025-07-02 00:59:39 +0800

    Website update

--- deploy_website.bat
+++ deploy_website.bat
@@ -1,7 +1,7 @@
 @echo off
-echo 正在自動部署...
 cd /d C:\Users\user\Desktop\web
 git add .
-git commit -m "一步到位網站更新"
+git commit -m "多層正式版網站更新"
 git push origin master
+echo 已更新至 https://herbert0220.github.io/solarfarmers.com/
 pause
