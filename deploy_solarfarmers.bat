c237e9f9e85425750ff8e1c3b4a30b55814ea169
Herbert Huamg <snake19820220@gmail.com>
2025-07-02 01:02:08 +0800

    更新最新多層正式版

--- deploy_solarfarmers.bat
+++
@@ -1,5 +0,0 @@
-
-@echo off
-cd /d "C:\Users\user\Desktop\web"
-"C:\Program Files\Git\git-bash.exe" -c "cd '/c/Users/user/Desktop/web' && git add . && git commit -m 'Website update' && git push origin master"
-pause
